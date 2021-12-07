57e0f69ea48c25a0f02cf9f7c1ce0c6262ba24aa
Analeh <bakic.helena@outlook.com>
2021-12-07 16:31:18 +0100

    fixed fig1

--- scripts/02-data-cleaning.R
+++ scripts/02-data-cleaning.R
@@ -31,22 +31,52 @@ df %>%
   filter(year > 1999) %>%
   group_by(year) %>% 
   summarise(n = n_distinct(title)) %>%
+  print(n = 30) 
   ggplot(aes(x = year, y = n)) +
   geom_line()
+  
+
 
 #total and gendered datasets per year - no gendered data sets anywhere but in 2018. 
 #but we don't have all datasets in the df, we need to figure it out better
-
+#we need to look into filtering geder out of multiple columns (title, description)
 
 df %>%
   mutate(year = as.integer(year)) %>%
   filter(year > 1999) %>%
-  mutate(gendered = if_else(str_detect(keyword, "weiblich|geschlecht"), "gendered", "not")) %>%
+  mutate(gendered = if_else(
+    str_detect(keyword, "weiblich|geschlecht|Geschlecht"), "gendered", "total")) %>%
   group_by(year, gendered) %>% 
   summarise(n = n_distinct(title)) %>%
   spread(gendered, n) %>%
   mutate(gendered = replace_na(gendered, 0)) %>%
   ggplot(aes(x = year)) +
   geom_line(aes(y = gendered), color = "red") +
-  geom_line(aes(y = not), color = "black")
+  geom_line(aes(y = total), color = "black")
+
+######the same figure as above (total and gendered datasets per year), but with looking for "gender" in several columns.
+#I didn't manage to figure it out if_else, so I filter it in one dataframe and use the other for total.
+
+year <- df %>%
+  mutate(year = as.integer(year)) %>%
+  filter(year > 1999) %>%
+  group_by(year) %>% 
+  summarise(n = n_distinct(title))  
+
+year_gender <- df %>%
+  mutate(year = as.integer(year)) %>%
+  filter(year > 1999) %>%
+  filter_at(.vars = vars(title, description, keyword),
+            .vars_predicate = any_vars(str_detect(., "weiblich|geschlecht|Geschlecht"))) %>%
+  group_by(year) %>%
+  summarise(n = n_distinct(title))
+
+fig1 <- full_join(year, year_gender, by = "year", suffix = c(".total", ".gendered"))
+fig1$n.gendered <- replace_na(fig1$n.gendered, 0)
+  
+fig1 %>%
+  ggplot(aes(x = year)) +
+  geom_line(aes(y= n.total), color = "black") +
+  geom_line(aes(y = n.gendered), color = "red")
+  
 
