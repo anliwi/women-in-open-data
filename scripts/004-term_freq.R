5a4952e0101b10926746bca323284853ba7ffb60
Analeh <bakic.helena@outlook.com>
2021-12-20 18:11:33 +0100

    Script cleaning and translations

--- scripts/004-term_freq.R
+++ scripts/004-term_freq.R
@@ -1,7 +1,6 @@
 #importing the data
-#if you want to replicate our findings please sleect the clean data file in the data folder within the repository
 
-data <- read.csv(choose.files())
+data <- read.csv("data/clean_data.csv")
 
 #installing the necessary libraries
 
@@ -45,54 +44,57 @@ names(keyword_freq) <-  c("term", "frequency")
 
 #selecting the 10 illustrative examples within the list of 100
 
-keyword_freq <-  keyword_freq[keyword_freq$term %in% c('bebauungsplan','strandbelegung', 'scharbeutz', 'futtermittel','pkw','lkw','bundesstrasse','geschlecht','weiblich', 'covid19'), ]
+keyword_freq1 <-  keyword_freq[keyword_freq$term %in% c('bebauungsplan','strandbelegung', 'scharbeutz', 'futtermittel','pkw','lkw','bundesstrasse','geschlecht','weiblich', 'covid19'), ]
 
-keyword_freq2 <-  keyword_freq[keyword_freq$term %in% c('bebauungsplan', 'tourismus'  ,'bodennutzung', 'futtermittel', 'strandbelegung','bundesstrasse','geschlecht', 'weiblich'), ]
+keyword_freq2 <-  keyword_freq[keyword_freq$term %in% c('bebauungsplan', 'tourismus','bodennutzung', 'futtermittel', 'strandbelegung','bundesstrasse','geschlecht', 'weiblich'), ] #keywords for alternative visualisation
 
 #plotting the 10 selected terms for illustrative purposes in a bar chart
 
-tf_plot <- ggplot(keyword_freq, 
+eng <- c("development plan", "beach occupacy", "Scharbeutz (city)", "animal feed", "personal vehicle", "truck", "national road", "gender", "female", "Covid 19" )
+keyword_freq1 <- cbind(keyword_freq1, eng)
+
+
+tf_plot <- ggplot(keyword_freq1, 
        aes(x=frequency, 
-           y=reorder(term, frequency))) +
+           y=reorder(eng, frequency))) +
   geom_point(color=c("#808080","#808080","#808080","#808080", "#808080", "#808080", "#808080","#800000", "#800000","#808080"), 
              size = 3) +
   geom_segment(aes(x = 40, 
                    xend = frequency, 
-                   y = reorder(term, frequency), 
-                   yend = reorder(term, frequency)),
+                   y = reorder(eng, frequency), 
+                   yend = reorder(eng, frequency)),
                color = "grey") +
   labs (x = "term frequency",
         y = "",
         title = "Term Frequency on govdata.de",
         subtitle = "Gotta set priorities",
         caption="as from over 55000 datasets") +
-  theme_gray(base_size=8)+ 
+  theme_minimal() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
 ggsave("tf_plot.png", width = 7, height = 6, dpi = 200, tf_plot)
 
 
-#Other possiblity
+
+################################################################################
+##############Alternative visualisation of words frequency, not used in the text
 
 library(ggrepel)
 
-
-
-d <- as.tibble(keyword_freq2[0:2])
-d <- d %>% mutate(x = 1,
-         term = as.character(term),
-         color = ifelse(str_detect(term, "geschlecht|weiblich"), "#800000", "#808080"))
+keywords_alt <- as.tibble(keyword_freq2[0:2])
+keywords_alt <- keywords_alt %>% mutate(term = as.character(term),
+         color = ifelse(str_detect(term, "geschlecht|weiblich"), "#800000", "#808080")) #marking gendered and nongendered words
   
-eng <- c("development plan", "tourism", "land use", "feed", "beach occupacy", "federal road", "gender", "female")
+eng2 <- c("development plan", "tourism", "land use", "animal feed", "beach occupacy", "national road", "gender", "female") #english translation
 
-dd <- cbind(d, eng)
+keywords_alt <- cbind(keywords_alt, eng2)
 
-fig3a <- ggplot(dd, aes(x = 1.001, y = frequency)) +
+fig3a <- ggplot(keywords_alt, aes(x = 1.001, y = frequency)) +
   scale_size(range = c(2, 12)) +
-  geom_text(aes(size = frequency, label = eng, color = color),
+  geom_text(aes(size = frequency, label = eng2, color = color),
                    hjust = 0) +
-  xlim(1, 1.1) +
+  xlim(1, 1.1) + #setting x-axis linits to fit long words
   scale_color_manual(values = c("#800000", "#808080")) +
   theme_minimal() +
   theme(legend.position = "none",
@@ -106,7 +108,7 @@ fig3a <- ggplot(dd, aes(x = 1.001, y = frequency)) +
   geom_segment(aes(x = 1.06, y = 1000, xend = 1.02, yend = 1000, color = "#800000"),
                arrow = arrow(length = unit(2, "mm"))) +
   geom_vline(xintercept = 1) +
-  labs(title = "Gotta set priorities",
-       y = "Term number in the portal")
+  labs(title = "Term number in the portal Govdata.de",
+       y = "Term number")
 
-ggsave("outputs/words.png", dpi = 400, fig3a)
+ggsave("outputs/tf-alternative.png", width = 7, height = 6, dpi = 200, fig3a)
